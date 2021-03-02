2ee30a0d6c36d7eca93f35b18bfea44b4438b4f1
Adrian Quej <Adrian Quej@LAPTOP-LCPEV7PP>
2021-03-01 22:20:39 -0600

    PRIMER COMMIT

---
+++ src/operadorespromedio59903/OperadoresPromedio59903.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorespromedio59903;
+
+/**
+ *
+ * @author Adrian Quej
+ */
+public class OperadoresPromedio59903 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Valores
+        
+        double primerValor = 5;
+        double segundoValor = 7;
+        double tercerValor = 23;
+        double cuartoValor = 14;
+        double quintoValor = 35;
+              
+                
+        // Calculando promedio
+                
+        double promedio = ((primerValor + segundoValor + tercerValor + cuartoValor + quintoValor)/5);
+        System.out.println("El promedio de los 5 valores es = " + promedio);
+    }
+    
+}
